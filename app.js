8fc4039d4faf66a00696c4c6ab42dc7c5b520929
mertfaikcigdem <mertfaikcigdem@gmail.com>
2024-04-30 23:38:16 +0300

    create middlewares

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+
+const myLogger = (req, res, next) => {
+  console.log('object');
+  next();
+};
+app.use(express.static('public'));
+app.use(myLogger);
 
 app.get('/', (req, res) => {
-  const photo = {
+  /* const photo = {
     id: 1,
     name: "Blog Title",
     description: "Blog description"
   };
-  res.send(photo);
+  res.send(photo); */
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
 app.listen(port, () => {
