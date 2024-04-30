5991b097f9608a2f5f49d64bf15290aac19c05cc
mertfaikcigdem <mertfaikcigdem@gmail.com>
2024-04-30 23:20:33 +0300

    installed nodemon

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const fn = () => {
-  console.log('working');
-};
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: "Blog Title",
+    description: "Blog description"
+  };
+  res.send(photo);
+});
+
+app.listen(port, () => {
+  console.log(`Ready on localhost:${port}`);
+});
