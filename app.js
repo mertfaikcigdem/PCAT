887cdca458f386009a05fba1f7c222434cdf7f15
mertfaikcigdem <mertfaikcigdem@gmail.com>
2024-05-25 00:38:50 +0300

    added edit page

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const fs = require('fs');
+const methodOverride = require('method-override');
 
 // models
 const Photo = require('./models/Photo');
@@ -11,8 +14,10 @@ const Photo = require('./models/Photo');
 // connect db
 mongoose.connect('mongodb://localhost/pcat-test-db');
 
+// template engine
 app.set('view engine', 'ejs');
 
+// middlewares
 const myLogger = (req, res, next) => {
   console.log('myLogger is started.');
   next();
@@ -21,9 +26,12 @@ app.use(express.static('public'));
 app.use(myLogger);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+app.use(methodOverride('_method'));
 
+// routes
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({});
+  const photos = await Photo.find({}).sort('-createdDate');
   res.render('index', {
     photos,
   });
@@ -48,9 +56,38 @@ app.get('/post/:id', async (req, res) => {
   });
 });
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
+  const uploadDir = 'public/uploads';
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+  let uploadedImage = req?.files?.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadedImage?.name;
+
+  uploadedImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadedImage?.name,
+    });
+  })
   res.redirect('/');
 });
+
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req?.params?.id });
+  res.render('edit', {
+    photo
+  });
+});
+
+app.put('/photo/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req?.params?.id });
+  console.log(req);
+  photo.title = req?.body?.title;
+  photo.description = req?.body?.description;
+  photo.save();
+  res.redirect(`/photo/${req?.params?.id}`);
+});
+
 
 app.listen(port, () => {
   console.log(`Ready on localhost:${port}`);
